12afb3374fbc03fafc574eb3de522e1ef56493c4
EricLeee <jialang.lee@gmail.com>
2017-02-05 23:10:47 -0500

    list interface update

--- CompressionManager/src/edu/ncsu/csc316/compression_manager/util/List.java
+++ CompressionManager/src/edu/ncsu/csc316/compression_manager/util/List.java
@@ -2,4 +2,13 @@ package edu.ncsu.csc316.compression_manager.util;
 
 public interface List {
 
+	int size();
+	
+	boolean contains(Object o); 
+	
+	void add(Object o);
+	
+	Object remove(int idx);
+	
+	int indexof(Object o);
 }
